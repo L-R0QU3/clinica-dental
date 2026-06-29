64e5c32d557a0d785bf5790f37980053d4e97863
L-R0QU3 <luis.roquetic@gmail.com>
2026-06-29 16:23:49 -0400

    Fix: usar mismo Service ID que el formulario de contacto

--- src/utils/emailjs.js
+++ src/utils/emailjs.js
@@ -2,7 +2,7 @@ import emailjs from '@emailjs/browser';
 
 const SERVICE_ID = 'service_2yo5x29';
 const TEMPLATE_ID = 'template_hiv5ddq';
-const PUBLIC_KEY = 'k0cDi3RpHrqf3-MzM';
+const PUBLIC_KEY = '1NsTm4lgVgfEZB76b';
 
 export const sendContactEmail = async (data) => {
   return emailjs.send(SERVICE_ID, TEMPLATE_ID, data, PUBLIC_KEY);
