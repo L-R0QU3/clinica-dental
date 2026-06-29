c9015f8ab6055290aee10538350d6346165a4f4d
L-R0QU3 <luis.roquetic@gmail.com>
2026-06-29 16:09:07 -0400

    Fix: agregar @emailjs/browser para frontend y regenerar lockfile

--- src/utils/emailjs.js
+++ src/utils/emailjs.js
@@ -1,9 +1,8 @@
 import emailjs from '@emailjs/browser';
 
-// Reemplaza con tus credenciales reales de EmailJS
-const SERVICE_ID = 'service_2yo5x29';      // ← tu Service ID
-const TEMPLATE_ID = 'template_hiv5ddq';    // ← tu Template ID
-const PUBLIC_KEY = 'k0cDi3RpHrqf3-MzM';         // ← tu Public Key
+const SERVICE_ID = 'service_2yo5x29';
+const TEMPLATE_ID = 'template_hiv5ddq';
+const PUBLIC_KEY = 'k0cDi3RpHrqf3-MzM';
 
 export const sendContactEmail = async (data) => {
   return emailjs.send(SERVICE_ID, TEMPLATE_ID, data, PUBLIC_KEY);
